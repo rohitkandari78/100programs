abe719934e2f7b2c836cecbef12e917890ca2fdd
Rohit Singh Kandari <rohitkandari148@gmail.com>
2026-08-19 21:33:20 +0530

    Add files via upload

--- 100programms.c
+++ 100programms.c
@@ -277,19 +277,28 @@ int main(){
      }
 
 
-  program 16.  -----> Write a program to read a character and cheak whether it is vowel or consonant.
+   // program 16.  -----> Write a program to read a character and cheak whether it is vowel or consonant.
 
   #include<stdio.h>
   int main(){
     char ch;
     
-    printf("Enter a character");
+    printf("Enter a character\n");
     scanf("%c",&ch);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U' ){
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U' ){
       printf("%c is a vowel",ch);
     }
     else{
-      printf("%c is a consonant");
+      printf("%c is a consonant",ch);
     }
-
   }
+
+
+
+      // program 17.----> Write a program to input a value and  check whether it is an alphabet,digit,special symbol.
+
+
+    #include<stdio.h>
+    int main(){
+
+    }
