d6ec520b7a9875a150cfe566e1155d704ec0d148
Rohit Singh Kandari <rohitkandari148@gmail.com>
2026-08-24 20:38:41 +0530

    Correct sum calculation in natural numbers program
    Fix logic for calculating the sum of natural numbers.

--- 100programms.c
+++ 100programms.c
@@ -486,11 +486,14 @@ int main(){
 
   #include<stdio.h>
   int main(){
-    int i,n,sum;
-    printf("Enter the value of n");
+    int i,n,sum=i;
+    printf("Enter the value of n\n");
     scanf("%d",&n);
-    for(i=0;i<=n;i++){
-    sum=i+
+     while(i<=n){
+        sum+1==sum;
+    }
+ for(i=0;i<=n;i++){
+    printf("%d\n",sum);
     }
   }
 
